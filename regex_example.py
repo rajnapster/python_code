13bd39e5321bbde48be5b29e4fca9eee68085621
rajnapster <raj.napster@gmail.com>
2015-01-30 12:02:20 +0530

    changed the content the file
    Took practice project from google repo

--- regex_example.py
+++ regex_example.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3.4
 
-'''
-Regex trial
-'''
+import os,re
 
-import re, os
-
-regex = '(?:^$)' #Non printing regex
-obj = re.compile(regex, re.I)
-
-print (os.getcwd())
-
-fd = open(r'chat_history.txt', mode='r', encoding='utf-8')
-for (no, line) in enumerate(fd):
-    if obj.match(line):
-        continue
-    print (line.strip('\n'))
-fd.close()
+def fileRead(fileName):
+    '''
+    line = '<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>'
+    (.*) helps in getting exactly the content required.
+    group(0) gives 1, group(1) gives Michael
+    '''
+    regex = r'<.*?><td>(.*)</td><td>(.*)</td><td>(.*)</td>'
+    matchObj = re.compile(regex, re.I)
+    fd = open(os.getcwd() + '\\babynames\\' + fileName, mode='r')
+    for line in fd:
+        z = matchObj.match(line.strip('\n'))
+        if z:
+            yield (z.group(2) + ' ' + z.group(1), z.group(3) + ' ' + z.group(1))
+    fd.close()
+if __name__=='__main__':
+    baby1990 = []
+    for line in fileRead('baby1990.html'):
+        baby1990.append(line[0])
+        baby1990.append(line[1])
+    print (baby1990)
