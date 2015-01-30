fbde7048ffe5a4afa5a63b7bf11cde6e0e5d5f5c
rajnapster <raj.napster@gmail.com>
2015-01-30 12:35:04 +0530

    Put the logic capturing year

--- regex_example.py
+++ regex_example.py
@@ -9,16 +9,24 @@ def fileRead(fileName):
     group(0) gives 1, group(1) gives Michael
     '''
     regex = r'<.*?><td>(.*)</td><td>(.*)</td><td>(.*)</td>'
+    regexYear = r'<.*?>\w+\s\w+\s([0-9]{4})<.*>'
     matchObj = re.compile(regex, re.I)
+    yearObj = re.compile(regexYear, re.I)
     fd = open(os.getcwd() + '\\babynames\\' + fileName, mode='r')
     for line in fd:
         z = matchObj.match(line.strip('\n'))
-        if z:
+        y = yearObj.match(line.strip('\n'))
+        if z and (len(z.groups()) == 3):
             yield (z.group(2) + ' ' + z.group(1), z.group(3) + ' ' + z.group(1))
+        elif y and (len(y.groups()) == 1):
+            yield y.group(1)
     fd.close()
 if __name__=='__main__':
     baby1990 = []
     for line in fileRead('baby1990.html'):
-        baby1990.append(line[0])
-        baby1990.append(line[1])
+        if len(line) == 2:
+            baby1990.append(line[0])
+            baby1990.append(line[1])
+        else:
+            baby1990.append(line)
     print (baby1990)
